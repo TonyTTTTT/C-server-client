35e99148322c3b45d5eeed7199c760f0e139a63e
TonyTTTTT <r09922176@csie.ntu.edu.tw>
2024-08-24 13:51:42 +0800

    complete parallel programming?

--- word_counter.cpp
+++ word_counter.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <fstream>
-#include <map>
+#include <unordered_map>
 #include <string>
 #include <string.h>
 #include <sstream>
@@ -10,6 +10,9 @@
 #include <numeric>
 #include <vector>
 #include <omp.h>
+#include <chrono>
+#include <windows.h>
+#include <winnt.h>
 
 #include "word_counter.h"
 
@@ -34,9 +37,11 @@ string read_file(string path) {
     return res;
 }
 
-vector<string> parallel_tokenize(string &str) {
-    vector<string> words;
-    vector<string> local_words[omp_get_max_threads()];
+void parallel_tokenize_and_count(string &str) {
+    static unordered_map<string, int> occurrence;
+    // vector<string> words;
+    // vector<string> local_words[omp_get_max_threads()];
+    int a = 0;
 
     #pragma omp parallel
     {
@@ -49,58 +54,105 @@ vector<string> parallel_tokenize(string &str) {
 
         while (start>0 && str[start-1] != ' ') start++;
 
-        while (end<str.size() && str[end] != ' ') end++;
+        while (end<str.size() && str[end-1] != ' ') end++;
 
         local_str = str.substr(start, end-start);
         istringstream stream(local_str);
         string word;
-
+    
         while (stream >> word) {
-            local_words[thread_id].push_back(word);
-            cout << word << endl;
+            // local_words[thread_id].push_back(word);
+            // cout << word << endl;
+            // #pragma omp critical
+            // {
+            //     occurrence[word]++;
+            // }
+            InterlockedIncrement((volatile long *)&occurrence[word]);
+
         }
     }    
 
-    for (int i=0; i<omp_get_max_threads(); i++) {
-        words.insert(words.end(), local_words[i].begin(), local_words[i].end());
-    }
+    // for (int i=0; i<omp_get_max_threads(); i++) {
+    //     words.insert(words.end(), local_words[i].begin(), local_words[i].end());
+    // }
 
-    return words;
-}
-
-void word_occurrence_count(string str) {
-    char delimit[] = " .,";
-    char *word;
-    char *cstr = &str[0];
-    
-    word = strtok(cstr, delimit);
-    #pragma omp parallel
-    {
-        while (word != NULL) {
-            cout << word << endl;
-            word = strtok(NULL, delimit);
-        }
-    }
-
-    vector<int> v = {2, 3, 4, 8};
-    int sum = reduce(v.begin(), v.end());
+    // for (auto itr : occurrence) {
+    //     printf("%s: %d\n", itr.first, itr.second);
+    // }
 
     return;
 }
+
+void tokenize_and_count(string &str) {
+    static unordered_map<string, int> occurrence;
+    // vector<string> words;
+
+    istringstream stream(str);
+    string word;
+
+    while (stream >> word) {
+        // words.push_back(word);
+        // cout << word << endl;
+        occurrence[word]++;
+    }
+
+    // for (auto itr : occurrence) {
+    //     printf("%s: %d\n", itr.first, itr.second);
+    // }
+
+    return;
+}
+
+// void word_occurrence_count(string str) {
+//     char delimit[] = " .,";
+//     char *word;
+//     char *cstr = &str[0];
+    
+//     word = strtok(cstr, delimit);
+//     #pragma omp parallel
+//     {
+//         while (word != NULL) {
+//             cout << word << endl;
+//             word = strtok(NULL, delimit);
+//         }
+//     }
+
+//     vector<int> v = {2, 3, 4, 8};
+//     int sum = reduce(v.begin(), v.end());
+
+//     return;
+// }
 
 void word_occurrence_count(char *path) {
     string path_str(path);
     string content = read_file(path_str);
 
-    parallel_tokenize(content);
-
-    // cout << content << endl;
+    parallel_tokenize_and_count(content);
 }
 
 int main() {
     char path[] = "./directory_big/file1";
-    word_occurrence_count(path);
+    string path_str(path);
+    string content = read_file(path_str);
 
+    // word_occurrence_count(path);
+    // word_occurrence_count("./directory_big/file1");
+
+    auto startTime = std::chrono::high_resolution_clock::now();
+    tokenize_and_count(content);
+    auto endTime = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> serialDuration = endTime - startTime;
+
+    startTime = std::chrono::high_resolution_clock::now();
+    parallel_tokenize_and_count(content);
+    endTime = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> parallelDuration = endTime - startTime;
+
+    // std::cout << "Serial result: " << resSerial << std::endl;
+    // std::cout << "Parallel result: " << resParallel << std::endl; 
+    std::cout << "Serial duration: " << serialDuration.count() << " seconds" << std::endl; 
+    std::cout << "Parallel duration: " << parallelDuration.count() << " seconds" << std::endl; 
+    std::cout << "Speedup: " << serialDuration.count() / parallelDuration.count() << std::endl; 
 
     return 0;
 }
